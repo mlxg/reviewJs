5687be9d99bc378670cd99d4c881aba3a7e1b980
xavier <clq1@qq.com>
2018-01-12 01:04:53 +0800

    mod:add

--- old/柯里化.js
+++ old/柯里化.js
@@ -1,4 +1,4 @@
-const curry1 = function (fn) {
+curry1 = function (fn) {
     return function (...args) {
         if (args.length >= fn.length) {
             return fn.apply(null, args);
@@ -9,6 +9,8 @@ const curry1 = function (fn) {
         }
     };
 };
+
+curry2 = fn => ((i) => (...args) => args.length >= i ? fn.apply(null, args) : (...args2) => fn.apply(null, args.concat(args2)))(fn.length);
 
 //一行柯里化
 curry = fn => (...args) => (...args2) => fn.apply(null, args.concat(args2));
